3c5197baeaf5908e15f9a2d8c1c388602ac587b2
haruto <mharu1018@outlook.jp>
2026-01-21 13:14:04 +0900

    音声再生用プログラム修正
    再生用プログラムをピンの情報に基づいて実行するように修正

--- src/test/audio_test.py
+++ src/test/audio_test.py
@@ -2,72 +2,57 @@ import json
 import time
 import pygame
 
-PIN_DATA = "../../assets/pin_data.json"
+from acquisition_module import get_loop_parameter
 
+#再生設定
 BPM = 180 #テンポ
 TOTAL_STEPS = 16 #1小節ごとのパターン数
 STEP_INTERVAL = 60 / BPM / 2
 
+volume_levels = [0.0, 0.3, 0.6, 1.0]
+
 pygame.mixer.init()
 
-# JSON読み込み
-with open(PIN_DATA, "r", encoding="utf-8") as f:
-    data = json.load(f)
+# sound_mapをJSONから構築
+PIN_DATA = "../../assets/pin_data.json"
 
-# volume_levels = data["volume_levels"]
+with open(PIN_DATA, "r", encoding="utf-8") as f:
+    pin_data = json.load(f)
 
 sound_map = {}
 
-# 楽器データをmapに加工
-for sound in data["sounds"]:
+for sound in pin_data["sounds"]:
 	sound_map[sound["instrument"]] = {
 		"sound": pygame.mixer.Sound(sound["file"]),
 		"patterns": {int(k): v for k, v in sound["patterns"].items()},
-		"complexity": 0,
-		"volume": 0
 	}
 
 
-def get_loop_parameters():
-	"""
-	1ループ分の入力を返す関数
-	[[instrument, complexity, volume], ...]
-	"""
-
-	# 今は仮の出力
-	return [
-		["hat", 0, 2],
-		["kick", 0, 0],
-		["snare", 0, 3]
-	]
-
-
-# 再生
-step = 0
-
+#再生ループ
 try:
+	step = 0
+
 	while True:
-		if step == 0:
-			params = get_loop_parameters()
+		loop_params = get_loop_parameter()
+			
+		for slot, param in enumerate(loop_params):
+            instrument, complexity, volume = param
 
-			for instrument, complexity, volume in params:
-				if instrument not in sound_map:
-					continue
+            # ピンなし
+            if instrument is None:
+                continue
 
-				sound = sound_map[instrument]
-				sound["complexity"] = complexity
-				sound["volume"] = volume
+            # JSONに存在しない楽器は無視
+            if instrument not in sound_map:
+                continue
 
-				sound["sound"].set_volume(1)
+            sound_info = sound_map[instrument]
+            pattern = sound_info["patterns"][complexity]
 
-			print("New loop params:", params)
-
-
-		for sound in sound_map.values():
-			pattern = sound["patterns"][sound["complexity"]]
-			if pattern[step] == 1:
-				# sound["sound"].set_volume(volume_levels[sound["volume"]])
-				sound["sound"].play()
+            if pattern[step % TOTAL_STEPS] == 1:
+                snd = sound_info["sound"]
+                snd.set_volume(volume_levels[volume])
+                snd.play()
 
 		step = (step + 1) % (TOTAL_STEPS * 2)
 		time.sleep(STEP_INTERVAL)
